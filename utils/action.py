bd662f9297deea44e549ba86623f6328e83a7752
Stephen Adewale <adewalestephennifemi@gmail.com>
2026-09-18 13:26:52 +0100

    feat: validated date input

--- utils/action.py
+++ utils/action.py
@@ -1,49 +1,73 @@
-from utils.component import clear_terminal
-import datetime
+from utils.component import clear_terminal, is_valid_date, validate_date, is_valid_time, validate_time, update_data
+from datetime import datetime
+
 def add_task():
   clear_terminal()
   print("Create a new Task")
 
   task_name = input("Task Title: ").strip()
   while task_name == "":
-    pass
+    task_name = input("Task Title (REQUIRED): ").strip()
 
   task_description = input("Task Description / Notes: ").strip()
   while task_description == "":
-    pass
+    task_description = input("Task Description / Notes (REQUIRED): ").strip()
 
-  has_due_date = input(f"Do you want to add a due date to Task {task_name}\nEnter [y/N]: ").strip()
+  has_due_date = input(f"Do you want to add a due date to Task {task_name}\nEnter [y/N]: ").strip().lower()
+  due_date = "N/A"
+  due_time = "N/A"
 
   while has_due_date in ["y", "yes"]:
-    now = datetime.date()
+    clear_terminal()
 
-    year_input = input("Year: ").strip()
-    month_input = input("Month: ").strip()
-    day_input = input("Day: ").strip()
+    due_date_input = input("Add your due date in this format [YYYY-MM-DD]: ").strip()
+    is_date_valid = is_valid_date(due_date_input)
 
-    """
-    Validate year, month and day
-    """
+    while is_date_valid is False:
+      clear_terminal()
+      print(f"'{due_date_input}' Invalid. Try Current/Future Date.")
+      due_date_input = input("Add your due date in this format [YYYY-MM-DD]: ").strip()
+      is_date_valid = is_valid_date(due_date_input)
 
-    recorded_date = {
-      "year": now.year(),
-      "month": now.month(),
-      "day": now.day(),
-    }
+    clear_terminal()
+    year, month, day = validate_date(due_date_input)
+
     due_date = {
-      "year": year_input,
-      "month": month_input,
-      "day": day_input,
+      "year": year,
+      "month": month,
+      "day": day,
+    }
+
+    due_time_input = input("Add your remainder time in this format [Hour-Mins]: ")
+    is_time_valid = is_valid_time(due_time_input)
+
+    while is_time_valid is False:
+      clear_terminal()
+      print("Invalid Time. Try a valid [Hour(0-23)-Mins(0-59)] time")
+      due_time_input = input("Add your remainder time in this format [Hour-Mins]: ")
+      is_time_valid = is_valid_time(due_time_input)
+
+    hour, mins = validate_time(due_time_input)
+    clear_terminal()
+    due_time = {
+      "hour": hour,
+      "mins": mins
     }
 
 
+  task = {
+    "name": task_name,
+    "description": task_description,
+    "has_due": has_due_date in ['yes', 'y'],
+    "due_info": {
+      "date": due_date,
+      "time": due_time,
+    },
+    "status": "",
+    "record_time": {}
+  }
 
-
-
-  """
-  Request Task Name
-  Request 
-  """
+  update_data(task)
 
 def view_tasks():
   clear_terminal()
--- utils/component.py
+++ utils/component.py
@@ -1,5 +1,62 @@
-import sys
 
 def clear_terminal():
+  import sys
   sys.stdout.write("\033[H\033[2J\033[3J")
   sys.stdout.flush()
+
+def is_valid_date(date_string):
+  from datetime import datetime
+
+  date_format="%Y-%m-%d"
+  try:
+    input_date = datetime.strptime(date_string, date_format).date()
+    today = datetime.today().date()
+    return input_date >= today
+
+  except ValueError:
+    return False
+
+def is_valid_time(time_string):
+  from datetime import datetime
+
+  time_format = "%H-%M" 
+  try:
+    datetime.strptime(time_string, time_format)
+    return True
+  except ValueError:
+    return False
+
+def validate_time(time_string):
+  hour, mins = time_string.split('-')
+  return hour, mins
+
+def validate_date(date_string):
+  year, month, day = date_string.split('-')
+  return year, month, day
+
+
+def update_data(task_data):
+  import json
+  json_file = "data/data.json"
+
+  try:
+    with open(json_file, "r") as file:
+      data = json.load(file)
+  except FileNotFoundError:
+    data = {}
+
+  if data:
+    next_id_int = max(int(key) for key in data.keys()) + 1
+  else:
+    next_id_int = 1
+
+  new_id = f"{next_id_int:03d}"
+  data[new_id] = task_data
+
+  with open(json_file, "w") as file:
+      json.dump(data, file, indent=2)
+
+  print()
+  print(f"Successfully added \"{task_data['name']}\" task with automatically generated ID: {new_id}")
+
+
