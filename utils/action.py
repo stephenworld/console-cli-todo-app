0dc55be67f8bd50adf54a7cc8cc56d29d5f60d87
Stephen Adewale <adewalestephennifemi@gmail.com>
2026-09-23 14:23:36 +0100

    feat: implemented status update

--- utils/action.py
+++ utils/action.py
@@ -1,9 +1,11 @@
-from utils.component import clear_terminal, is_valid_date, validate_date, is_valid_time, validate_time, update_data, load_tasks
+from utils.component import clear_terminal, is_valid_date, validate_date, is_valid_time, validate_time, update_data, load_tasks, print_tasks
 from datetime import datetime
+
+json_file = "data/data.json"
 
 def add_task():
   clear_terminal()
-  print("Create a new Task")
+  print("Creating a new Task\n")
 
   task_name = input("Task Title: ").strip()
   while task_name == "":
@@ -13,7 +15,8 @@ def add_task():
   while task_description == "":
     task_description = input("Task Description / Notes (REQUIRED): ").strip()
 
-  has_due_date = input(f"Do you want to add a due date to Task {task_name}\nEnter [y/N]: ").strip().lower()
+  print()
+  has_due_date = input(f"Do you want to add a due date to Task '{task_name}'\nEnter [y/N]: ").strip().lower()
   due_date = "N/A"
   due_time = "N/A"
 
@@ -34,9 +37,9 @@ def add_task():
     year, month, day = validate_date(due_date_input)
 
     due_date = {
-      "year": year,
-      "month": month,
-      "day": day,
+      "year": str(year),
+      "month": str(month),
+      "day": str(day),
     }
 
     due_time_input = input("Add your remainder time in this format [Hour-Mins]: ")
@@ -51,8 +54,8 @@ def add_task():
     hour, mins = validate_time(due_time_input)
     clear_terminal()
     due_time = {
-      "hour": hour,
-      "mins": mins
+      "hour": str(hour),
+      "mins": str(mins)
     }
 
     break
@@ -90,6 +93,7 @@ def add_task():
     "status": status
   }
 
+  clear_terminal()
   update_data(task)
 
 
@@ -99,30 +103,17 @@ def filter_task_by_status(status, tasks):
     print(f"There isn't any tasks with the {status} status")
     return
 
-  print("-"*110)
-  print(f"| {"ID":<5} | {"Task Name":<25} | {"Creation Date":<25} | {"Due Date":<25} | {"Status":<15} |")
-  print("-"*110)
-
-  for task_id, task in filtered_task:
-    task_name = task['name']
-    task_creation = task['creation']['date']['year'] + "-" + task['creation']['date']['month'] + "-" + task['creation']['date']['day']
-    has_due_date = task['has_due']
-    dues_date = "N/A"
-    if has_due_date:
-      dues_date = task['due_info']['date']['year'] + "-" + task['due_info']['date']['month'] + "-" + task['due_info']['date']['day']
-    status = task['status']
-
-    print(f"| {task_id:<5} | {task_name:<25} | {task_creation:<25} | {dues_date:<25} | {status:<15}")
-
+  print_tasks(filtered_task)
 
 def view_tasks():
   clear_terminal()
   actions = ["view all task", "Filter by status"]
-
+  print("View / Filter Task\n")
   for idx, action in enumerate(actions, 1):
-    print(f"{idx}. {action}")
+    print(f"[{idx}] {action}")
 
   tasks = load_tasks()
+  print()
   user_action = input("Pick an action: ").strip()
 
   while user_action not in ["1", "2"]:
@@ -136,27 +127,17 @@ def view_tasks():
       print("Task is empty.")
       return
 
-    print("-"*110)
-    print(f"| {"ID":<5} | {"Task Name":<25} | {"Creation Date":<25} | {"Due Date":<25} | {"Status":<15} |")
-    print("-"*110)
-
-    for task_id, task in tasks.items():
-      task_name = task['name']
-      task_creation = task['creation']['date']['year'] + "-" + task['creation']['date']['month'] + "-" + task['creation']['date']['day']
-      has_due_date = task['has_due']
-      dues_date = "N/A"
-      if has_due_date:
-        dues_date = task['due_info']['date']['year'] + "-" + task['due_info']['date']['month'] + "-" + task['due_info']['date']['day']
-      status = task['status']
-
-      print(f"| {task_id:<5} | {task_name:<25} | {task_creation:<25} | {dues_date:<25} | {status:<15} |")
+    print("Showing All Available tasks\n")
+    print_tasks(tasks.items())
 
   elif user_action == "2":
     clear_terminal()
     statuses = ["later", "undated", "completed"]
+    print("Filter by Status\n")
     for idx, status in enumerate(statuses, 1):
-      print(f"{idx} {status.capitalize()}")
+      print(f"[{idx}] {status.capitalize()}")
 
+    print()
     user_action = input("Select from the available statuses to filter: ").strip()
     clear_terminal()
 
@@ -165,24 +146,135 @@ def view_tasks():
       user_action = input("Select from the available statuses to filter: ").strip()
 
     if user_action == "1":
+      print("Filtering tasks scheduled for LATER\n")
       filter_task_by_status("later", tasks)
 
     elif user_action == "2":
+      print("Filtering by UNDATED tasks\n")
       filter_task_by_status("undated", tasks)
 
     elif user_action == "3":
+      print("Filtering by COMPLETED tasks\n")
       filter_task_by_status("completed", tasks)
-
-
 
 
 def complete_task():
   clear_terminal()
-  print("Complete Task")
+  tasks = load_tasks()
+
+  if not tasks:
+    print("Task is empty.")
+    return
+
+  print("Update task status\n")
+  print_tasks(tasks.items())
+  print()
+  user_action = input("Select a task ID that has been completed: ").strip()
+  while user_action == "":
+    print("Task can't be empty")
+    user_action = input("Select a task ID that has been completed: ").strip()
+
+  available_task_id = [id for id in tasks]
+  while user_action not in available_task_id:
+    print(f"Invalid task ID try any of these ID's {available_task_id}")
+    user_action = input("Select a task ID that has been completed: ").strip()
+
+  print()
+  certain_action = input(f"Are you sure you want to mark '{tasks[user_action]["name"]}' as completed [y/N]: ").strip()
+  clear_terminal()
+  if certain_action in ["yes", "y"]:
+    tasks[user_action]["status"] = "completed"
+    import json
+    with open(json_file, "w") as file:
+      json.dump(tasks, file, indent=2)
+    print(f"Task '{user_action}' with the name '{tasks[user_action]["name"]}' has been completed")
+  else:
+    print("Action cancled")
 
 def edit_task():
   clear_terminal()
-  print("Edit task")
+  tasks = load_tasks()
+  if not tasks:
+    print("Task is empty.")
+    return
+
+  print_tasks(tasks.items())
+  print()
+  user_action = input("Select a task ID you want to edit: ").strip()
+  while user_action == "":
+    print("Task can't be empty")
+    user_action = input("Select a task ID you want to edit: ").strip()
+
+  available_task_id = [id for id in tasks]
+  while user_action not in available_task_id:
+    print(f"Invalid task ID try any of these ID's {available_task_id}")
+    user_action = input("Select a task ID you want to edit: ").strip()
+
+  task_data = tasks[user_action]
+
+  clear_terminal()
+  print(f"Showing full details for task {user_action}\n")
+  
+  name = task_data["name"]
+  description = task_data["description"]
+  creation = task_data["creation"]
+  has_due = task_data["has_due"]
+  due = task_data["due_info"]
+
+  creation_full_date = f"{creation["date"]["year"]}-{creation["date"]["month"]}-{creation["date"]["day"]}"
+  creation_time = f"{creation["time"]["hour"]}-{creation["time"]["mins"]}"
+
+  print(f"Task Name: {name}")
+  print(f"Task Description: {description}")
+  print(f"Creation Date / Time: {creation_full_date} / {creation_time}")
+
+  available_edit_options = [ "Name", "Description", "Creation" ]
+
+  if has_due:
+    due_full_date = f"{due["date"]["year"]}-{due["date"]["month"]}-{due["date"]["day"]}"
+    due_time = f"{due["time"]["hour"]}-{due["time"]["mins"]}"
+    print(f"Due Date / Time: {due_full_date} / {due_time}")
+    available_edit_options = [ "Name", "Description", "Creation", "Due Date" ]
+
+  print()
+  print("Things editable")
+
+  for idx, option in enumerate(available_edit_options, 1):
+    print(f"[{idx}] {option}")
+
+  print()
+  action = input("What would you edit: ").strip()
+  actions = [str(idx) for idx,_ in enumerate(available_edit_options, 1)]
+
+  while action not in actions:
+    print(f"Valid actions are {actions}")
+    action = input("What would you edit: ").strip()
+
+  if action == "1":
+    """
+    Edit task Name
+    """
+    print("Edit task Name")
+  elif action == "2":
+    """
+    Edit task Description
+    """
+    print("Edit task Description")
+  elif action == "3":
+    """
+    Edit task creation date
+    """
+    print("Edit task creation date")
+  elif action == "4":
+    """
+    Edit task Due Date
+    """
+    print("Edit task Due Date")
+  else:
+    print("Invalid Actions")
+
+  
+
 
 def exit():
   clear_terminal()
