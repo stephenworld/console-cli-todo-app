8828c355560a0c6a1d000c35cc919bd21e7701d0
Stephen Adewale <adewalestephennifemi@gmail.com>
2026-09-23 14:24:58 +0100

    feat: added print_tasks function to enhance reusability

--- utils/component.py
+++ utils/component.py
@@ -29,11 +29,11 @@ def is_valid_time(time_string):
 
 def validate_time(time_string):
   hour, mins = time_string.split('-')
-  return hour, mins
+  return int(hour), int(mins)
 
 def validate_date(date_string):
   year, month, day = date_string.split('-')
-  return year, month, day
+  return int(year), int(month), int(day)
 
 
 def update_data(task_data):
@@ -61,8 +61,45 @@ def load_tasks():
   try:
     with open(json_file, "r") as file:
       data = json.load(file)
+      data = update_task_status(data)
+      
   except FileNotFoundError:
     data = {}
 
   return data
 
+def update_task_status(data):
+  for _, task in data.items():
+    has_due_date = task["has_due"]
+    due_data = task["due_info"]
+    creation = task["creation"]
+
+    if has_due_date:
+      creation_full_date = f"{creation["date"]["year"]}-{creation["date"]["month"]}-{creation["date"]["day"]}"
+      creation_time = f"{creation["time"]["hour"]}-{creation["time"]["mins"]}"
+
+      due_full_date = f"{due_data["date"]["year"]}-{due_data["date"]["month"]}-{due_data["date"]["day"]}"
+      due_time = f"{due_data["time"]["hour"]}-{due_data["time"]["mins"]}"
+
+      if (creation_full_date > due_full_date or creation_time > due_time) and task["status"] != "completed":
+        task["status"] = "overdue"
+
+  return data
+
+
+def print_tasks(tasks):
+  print("-"*110)
+  print(f"| {"ID":<5} | {"Task Name":<25} | {"Creation Date":<25} | {"Due Date":<25} | {"Status":<15} |")
+  print("-"*110)
+
+  for task_id, task in tasks:
+    task_name = task['name']
+    task_creation = task['creation']['date']['year'] + "-" + task['creation']['date']['month'] + "-" + task['creation']['date']['day']
+    has_due_date = task['has_due']
+    dues_date = "N/A"
+
+    if has_due_date:
+      dues_date = task['due_info']['date']['year'] + "-" + task['due_info']['date']['month'] + "-" + task['due_info']['date']['day']
+    status = task['status']
+
+    print(f"| {task_id:<5} | {task_name:<25} | {task_creation:<25} | {dues_date:<25} | {status:<15} |")
